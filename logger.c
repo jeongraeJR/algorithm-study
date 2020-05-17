5efa64f87b478894724ebd7de7821eb69a70ba6a
jeongraeJR <hwal92@naver.com>
2020-05-17 14:18:09 +0900

    로거 수정

--- logger.c
+++ logger.c
@@ -8,39 +8,40 @@ void push(char *stack, int value, int *top){
 
 char pop(char *stack, int *top) {
     if(*top == -1) exit(0);
-    return stack[*top--];
+    return stack[(*top)--];
 }
 
 
 int main(){
     int left_top = -1;
     int right_top = -1;
-    char *left_stack = (char*)malloc(sizeof(char) * 1000000);
-    char *right_stack = (char*)malloc(sizeof(char) * 1000000);
+   
     char *data =  (char*)malloc(sizeof(char) * 1000000);
 
     scanf("%s",data);
+
+    char *left_stack = (char*)malloc(sizeof(char) * strlen(data));
+    char *right_stack = (char*)malloc(sizeof(char) * strlen(data));
 
     for(int i=0; i<strlen(data); i++){
         if(data[i]=='-')
         {
             if(left_top!=-1){
-                 pop(left_stack, &left_top);
+                pop(left_stack, &left_top);
             }
         }
         else if(data[i]=='<')
         {   
            
             if(left_top!=-1){
-                 printf("%d", pop(left_stack, &left_top));
-                 //push(right_stack, pop(left_stack, &left_top), &right_top);
+                push(right_stack, pop(left_stack, &left_top), &right_top);
             }
             
         }
         else if(data[i]=='>' )
         {
             if(right_top!=-1){
-                 push(left_stack, pop(right_stack, &right_top), &left_top);
+                push(left_stack, pop(right_stack, &right_top), &left_top);
             }
         }
         else {
@@ -48,11 +49,15 @@ int main(){
         }
 
     }
-      printf("result test %d", left_top);
-
-    for(int i=0; i<=left_top; i++){
+ 
+    for(int i=0; i<=left_top; i++)
+    {
         printf("%c",left_stack[i]);
     }
-    printf("right stack: %s",right_stack);
+
+    for(int i=right_top; i>=0; i--)
+    {
+        printf("%c",right_stack[i]);
+    }
     return 0;
 }
